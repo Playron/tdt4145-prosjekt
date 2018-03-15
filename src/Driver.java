1b34b2262abed2e92492771a0f3c010470f9e73f
perdh <perdh@stud.ntnu.no>
2018-03-15 14:46:49 +0100

    Lagt til user

--- src/Driver.java
+++ src/Driver.java
@@ -9,7 +9,7 @@ public class Driver {
 			final String var1 ="Fredrik";
 			final String var2 = "Nordstad";
 			final String epost = "fredrikdn@gmail.com";
-			Connection myconn1 = DriverManager.getConnection("jdhc:mysql://localhost:3306/tdt4145prosjekt", "tdt4145prosjekt", "smud");
+			Connection myconn1 = DriverManager.getConnection("jdhc:mysql://localhost:3306/tdt4145prosjekt", "pertest", "test");
 			PreparedStatement statement = myconn1.prepareStatement("INSERT INTO employee(id, last_name, first_name, email) VALUES ('"+id+"', '"+var1+"', '"+var2+", '"+epost+"')" );
 			System.out.println(statement);
 			statement.executeUpdate();
@@ -36,20 +36,20 @@ public class Driver {
 		try {
 			
 			//Get connection to database
-			Connection myconn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tdt4145prosjekt", "tdt4145prosjekt", "smud");
+			Connection myconn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tdt4145prosjekt", "pertest", "test");
 			
 			//Create a statement
-			//Statement myStatement = myconn.createStatement();
+			Statement myStatement = myconn.createStatement();
 			//Statement newStatement = myconn.createStatement();
 			//Excecute sql Query
-			//ResultSet res = myStatement.executeQuery("select * from employee");
-			PreparedStatement statement = myconn.prepareStatement("INSERT INTO employee(id, last_name, first_name, email) VALUES ('"+id+"', '"+var1+"', '"+var2+", '"+epost+"')" );
+			ResultSet res = myStatement.executeQuery("select * from apparat");
+			//PreparedStatement statement = myconn.prepareStatement("INSERT INTO employee(id, last_name, first_name, email) VALUES ('"+id+"', '"+var1+"', '"+var2+", '"+epost+"')" );
 			//ResultSet test  = newStatement.executeQuery("")
-			statement.executeUpdate();
+			//statement.executeUpdate();
 			//Process the rusult set
-			//while(res.next()) {
-			//	System.out.println(res.getString("id") + ", " +  res.getString("last_name") + ", " +  res.getString("first_name"));
-			//}
+			while(res.next()) {
+				System.out.println(res.getString("apparatID") + ", " +  res.getString("navn") + ", " +  res.getString("beskrivelse"));
+			}
 		}
 		catch(Exception e){
 			e.printStackTrace();
