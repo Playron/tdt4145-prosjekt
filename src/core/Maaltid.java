13fb3d99a66a3a1a79428003b00e0fbfe9942697
Sondre <Sondre@10.22.31.180>
2018-03-15 16:29:37 +0100

    Klasser

---
+++ src/core/Maaltid.java
@@ -0,0 +1,17 @@
+package core;
+
+import java.sql.Date;
+
+public class Maaltid {
+	private Date dato;
+
+	public Date getDato() {
+		return dato;
+	}
+
+	public void setDato(Date dato) {
+		this.dato = dato;
+	}
+	
+
+}
