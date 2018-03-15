69229bd8bbec7bbbd07ef445e062a0fb87828171
Sondre <Sondre@10.22.31.180>
2018-03-15 14:52:58 +0100

    Person med getters setters

---
+++ src/Person.java
@@ -0,0 +1,40 @@
+
+public class Person {
+	
+//	private int profilID;
+	private String navn;
+	private int vekt;
+	private int hvilepuls;
+	private int personligForm;
+	
+//	public void setProfilID(int profilID) {
+//		this.profilID = profilID;
+//	}
+//	public int getProfilID() {
+//		return profilID;
+//	}
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
+	public String getNavn() {
+		return navn;
+	}
+	public int getVekt() {
+		return vekt;
+	}
+	public void setVekt(int vekt) {
+		this.vekt = vekt;
+	}
+	public void setHvilepuls(int hvilepuls) {
+		this.hvilepuls = hvilepuls;
+	}
+	public int getHvilepuls() {
+		return hvilepuls;
+	}
+	public void setPersonligForm(int personligForm) {
+		this.personligForm = personligForm;
+	}
+	public int getPersonligForm() {
+		return personligForm;
+	}
+}
