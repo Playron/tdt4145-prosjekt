a05cf18bcc3d48e39c6477d16b1dbff55b0e413a
perdh <perdh@stud.ntnu.no>
2018-03-16 13:41:32 +0100

    Added functionality to add treningsøkt

--- src/core/Driver.java
+++ src/core/Driver.java
@@ -1,5 +1,8 @@
 package core;
 import java.sql.*;
+import java.text.Format;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 public class Driver {
 	
@@ -60,6 +63,7 @@ public class Driver {
 		System.out.println("Connecting to a selected database...");
 		Connection myconn = DriverManager.getConnection(database, "pertest", "test");
 		System.out.println("Connected database successfully...");
+		System.out.println(myconn.getCatalog());
 		
 		
 		PreparedStatement stmt = myconn.prepareStatement("INSERT into apparat(apparatID, navn, beskrivelse) VALUES(?,?,?)");
@@ -71,6 +75,39 @@ public class Driver {
 		System.out.println("Trying to insert Apparat");
 		
 		stmt.executeUpdate();
+		
+	}
+	
+	public void leggTilØvelse() throws SQLException, ParseException {
+		Scanner scanner = new Scanner(System.in);
+		Connection myconn = DriverManager.getConnection(database, "pertest", "test");
+		System.out.println("Connecting to Database:" + myconn.getCatalog() + "......");
+		System.out.println("Connection is established!");
+		
+		System.out.println("Skriv inn treningsøktID: ");
+		int treningsøktID = scanner.nextInt();
+		
+		
+		
+		SimpleDateFormat date = new SimpleDateFormat("yyyyMMdd");
+		System.out.println("Skriv inn Dato: (Form: yyyyMMdd) ");
+		Date parsed = (Date) date.parse(scanner.next());
+		java.sql.Date sql = new java.sql.Date(parsed.getTime());
+		
+		
+		
+		System.out.println("Skriv inn tidspunkt: ");
+		System.out.println("Skriv inn varighet: ");
+		System.out.println("Skriv inn prestasjon: ");
+		System.out.println("Skriv inn notat: ");
+		
+		
+		
+		PreparedStatement stmt = myconn.prepareStatement("INSERT INTO treningsøkt(treningsøktid, dato, tidspunkt, varighet, prestasjon, notat) VALUES(?,?,?,?,?,?)");
+		stmt.setInt(1, treningsøktID);
+		stmt.setDate(2, sql);
+		
+		
 		
 	}
 	
